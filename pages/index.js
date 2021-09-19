8c25e9b7fe599bf112813aef77d54d3136822664
Abiola Adewale <princehaybee@gmail.com>
2021-09-19 23:02:53 +0200

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,37 @@
-export default function Recipes() {
+import { createClient } from "contentful";
+import CaseCard from "../components/CaseCard";
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: "l77qha1voe0c",
+    accessToken: "l_DgqUQDUPFySs6W0Av6c9qTbQU-X5vRxDEIRfQMfTU",
+  });
+
+  const res = await client.getEntries({ content_type: "caseStudy" });
+
+  return {
+    props: {
+      studies: res.items,
+    },
+  };
+}
+
+export default function Recipes({ studies }) {
+  console.log(studies);
+
   return (
     <div className="recipe-list">
-      Recipe List
+      {studies.map((study) => (
+        <CaseCard key={study.sys.id} study={study} />
+      ))}
+
+      <style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 60px;
+        }
+      `}</style>
     </div>
-  )
+  );
 }
--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,95 @@
-export default function RecipeDetails() {
+import { createClient } from "contentful";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Image from "next/image";
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({
+    content_type: "caseStudy",
+  });
+
+  const paths = res.items.map((item) => {
+    return {
+      params: { slug: item.fields.slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const { items } = await client.getEntries({
+    content_type: "caseStudy",
+    "fields.slug": params.slug,
+  });
+
+  return {
+    props: { study: items[0] },
+  };
+};
+
+export default function StudyDetails({ study }) {
+  const { featuredImage, title, time, caseStudyData, method } = study.fields;
+  console.log(study);
+
   return (
     <div>
-      Recipe Details
+      <div className="banner">
+        <Image
+          src={"https:" + featuredImage.fields.file.url}
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
+        />
+        <h2>{title}</h2>
+      </div>
+
+      <div className="info">
+        <p>Takes about {time} weeks to finish.</p>
+        <h3>Data:</h3>
+
+        {caseStudyData.map((casedat) => (
+          <span key={casedat}>{casedat}</span>
+        ))}
+      </div>
+
+      <div className="method">
+        <h3>Method:</h3>
+        <div>{documentToReactComponents(method)}</div>
+      </div>
+
+      <style jsx>{`
+        h2,
+        h3 {
+          text-transform: uppercase;
+        }
+        .banner h2 {
+          margin: 0;
+          background: #fff;
+          display: inline-block;
+          padding: 20px;
+          position: relative;
+          top: -60px;
+          left: -10px;
+          transform: rotateZ(-1deg);
+          box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+        }
+        .info p {
+          margin: 0;
+        }
+        .info span::after {
+          content: ", ";
+        }
+        .info span:last-child::after {
+          content: ".";
+        }
+      `}</style>
     </div>
-  )
+  );
 }
