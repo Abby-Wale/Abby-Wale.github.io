27d345bdfc7222ad722467831bf725db5957ff97
Abiola Adewale <princehaybee@gmail.com>
2021-09-21 22:00:27 +0200

    resume added as pdf

--- pages/designs/[slug].js
+++ pages/designs/[slug].js
@@ -1,7 +1,6 @@
 import { createClient } from "contentful";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import Image from "next/image";
-import Preview from "preview-office-docs";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
--- pages/resume.js
+++ pages/resume.js
@@ -1,24 +1,29 @@
 import React, { useState } from "react";
-import Pdf from "@mikecousins/react-pdf";
-import styled from "@emotion/styled";
 
-const resume = () => {
-  const [page, setPage] = useState(1);
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+function resume() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
 
   return (
-    <Main>
-      <Pdf file="AdewaleAbiolaResume.pdf" page={page} />
-    </Main>
+    <div>
+      <Document
+        file="AdewaleAbiolaResume.pdf"
+        size="A4"
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+    </div>
   );
-};
-
+}
 export default resume;
-
-export const Main = styled.div`
-  padding: 100px 0;
-  background-color: #064734;
-  color: #fff;
-
-  @media screen and (min-width: 800px) {
-  }
-`;
