e92d6c4ed161760526de6bd0a090f13facdfedb3
Abby-Wale <abiola.aremuade@gmail.com>
2023-09-16 11:50:11 +0100

    fix hydration mismatch using useEffect

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
+import { useState, useEffect } from 'react';
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);  // set isClient to true when component is mounted
+  }, []);
+
   return (
     <Layout>
-      <Component {...pageProps} />
+      {isClient ? <Component {...pageProps} /> : <div>Loading...</div>}
     </Layout>
   );
 }
