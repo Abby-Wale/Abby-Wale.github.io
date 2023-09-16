946d42045e40156836c3c8f124489442c802b98d
Abby-Wale <abiola.aremuade@gmail.com>
2023-09-16 11:41:14 +0100

    fix layout

--- components/Layout.js
+++ components/Layout.js
@@ -8,53 +8,36 @@ export default function Layout({ children }) {
     <div className="layout">
       <Navbar />
       <header>
-        <Link href="/">
-
-          <h1>
-            <span>User Experience</span>
-            <span>Design</span>
-          </h1>
-          {/* <h3>enjoy it</h3> */}
-
+        <Link href="/" passHref>
+          <a>
+            <h1>
+              <span>Data Scientist and AI Engineer</span>
+            </h1>
+          </a>
         </Link>
       </header>
 
-      <div className="page-content">{children}</div>
+      <div className="page-content">
+        {children}
+      </div>
 
       <Footer>
         <Footer1>
-          <a
-            href="https://github.com/princehaybee"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <SocialIcon href="https://github.com/Abby-Wale" target="_blank" rel="noopener noreferrer">
             <FaGithub color="#fff" />
-          </a>
-          <a
-            href="https://twitter.com/bornriches"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          </SocialIcon>
+          <SocialIcon href="https://twitter.com/bornriches" target="_blank" rel="noopener noreferrer">
             <FaTwitter color="#fff" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/waleabiola/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          </SocialIcon>
+          <SocialIcon href="https://www.linkedin.com/in/waleabiola/" target="_blank" rel="noopener noreferrer">
             <FaLinkedinIn color="#fff" />
-          </a>
+          </SocialIcon>
         </Footer1>
-
         <Footer2>
           whatsapp contact:
-          <a
-            href="https://wa.me/4915217431500"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <SocialIcon href="https://wa.me/4915217431500" target="_blank" rel="noopener noreferrer">
             <FaWhatsapp color="#fff" />
-          </a>
+          </SocialIcon>
         </Footer2>
       </Footer>
     </div>
@@ -66,37 +49,35 @@ export const Footer = styled.div`
   flex-direction: column;
   text-align: center;
   padding: 20px 0;
-
-  left: 0;
-  bottom: 0;
   a {
     text-decoration: none;
     font-size: 25px;
     margin: 0 15px;
   }
 `;
+
 export const Footer1 = styled.div`
-  display: block;
   text-align: center;
   padding: 20px 0;
-
-  left: 0;
-  bottom: 0;
   a {
     text-decoration: none;
     font-size: 25px;
     margin: 0 15px;
   }
 `;
+
 export const Footer2 = styled.div`
   text-align: center;
   padding: 10px 0;
   color: #fff;
-
-  left: 0;
-  bottom: 0;
   a {
     text-decoration: none;
     font-size: 25px;
   }
+`;
+
+export const SocialIcon = styled.a`
+  text-decoration: none;
+  font-size: 25px;
+  margin: 0 15px;
 `;
